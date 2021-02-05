627d700381a8d00cb4649b3adea237a50922b4a5
Muhammet Ali Yahşi <alleyy@outlook.com.tr>
2021-02-05 18:52:31 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>()
+            {
+                new Car{Id=1, BrandId=2, ColorId=3, ModelYear=2010 ,DailyPrice=150, Description="1.6"},
+                new Car{Id=2, BrandId=2, ColorId=1, ModelYear=2008 ,DailyPrice=100, Description="1.3"},
+                new Car{Id=3, BrandId=3, ColorId=1, ModelYear=2008 ,DailyPrice=125, Description="1.5"},
+                new Car{Id=4, BrandId=4, ColorId=2, ModelYear=2020 ,DailyPrice=250, Description="1.8"},
+                new Car{Id=5, BrandId=4, ColorId=4, ModelYear=2019 ,DailyPrice=200, Description="1.6"},
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(p=> p.Id == car.Id);
+
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int Id)
+        {
+            Car car = _cars.Find(c => c.Id == Id);
+            return car;
+            //return _cars.Where(p => p.Id == Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(p => p.Id == car.Id);
+
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
