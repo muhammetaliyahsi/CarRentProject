4b28809fc5c49dee98df0dc69ef44fd0d01f27a1
Muhammet Ali Yahşi <alleyy@outlook.com.tr>
2021-02-07 12:04:13 +0300

    .

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -9,5 +9,11 @@ namespace Business.Abstract
     {
         List<Car> GetAll();
         Car GetById(int Id);
+        List<Car> GetCarsByBrandId(int id);
+        List<Car> GetCarsByColorId(int id);
+        void Add(Car car);
+        void Update(Car car);
+        void Delete(Car car);
+
     }
 }
--- Console/Program.cs
+++ Console/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace Console
@@ -8,14 +10,25 @@ namespace Console
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            //carManager.Add(new Car
+            //{
+            //    Id = 2,
+            //    BrandId = 1,
+            //    ColorId = 1,
+            //    DailyPrice = 150,
+            //    ModelYear = 2010,
+            //    Description = "1.6 Model"
+            //});
 
             foreach (var car in carManager.GetAll())
             {
                 System.Console.WriteLine(car.Description);
             }
 
-            System.Console.WriteLine(carManager.GetById(2).Description);
         }
     }
 }
